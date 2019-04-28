215b178636bcaf18a1988445f07d7cf057527c6a
Vadim Efremov <ottofonsmorc@yandex.ru>
2019-04-29 03:08:03 +0700

    Completion menu

--- menu.c
+++ menu.c
@@ -4,27 +4,37 @@
 int main()
 {
     int choice;
-	while (1)
-	{
-		system("cls");
-		printf("1:new game\n");
-		scanf("%d", &choice);
-		system("cls");
+    while (1)
+    {
+        //system("cls");
+        printf("1:new game\n");
+        printf("2:Game rules\n");
+		printf("3:Exit\n");
+        scanf("%d", &choice);
+        //system("cls");
 
-		if (choice == 1)
-		{
-			int choise1;
-			system("cls");
-			printf("Select difficulty level:\n");
-			printf("1:Easy(4 numbers)\n");
-			printf("2:Medium(6 numbers)\n");
-			printf("3:Hard(8 numbers)\n");
-			scanf("%d", &choise1);
-			system("cls");
+        if (choice == 1)
+        {
+            int choise1;
+           // system("cls");
+            printf("Select difficulty level:\n");
+            printf("1:Easy(4 numbers)\n");
+            printf("2:Medium(6 numbers)\n");
+            printf("3:Hard(8 numbers)\n");
+            scanf("%d", &choise1);
+          //  system("cls");
         }
-        if ((choice != 1))
-			break;
-        system("PAUSE");
+        else if (choice == 2)
+        {
+         printf("Game rules: ***\n");
+        }
+        else if (choice == 3)
+        {
+            printf("***\n");
+		}
+		if ((choice != 1) && (choice != 2))
+		break;
+        //system("PAUSE");
     }
-        system("PAUSE");
+       // system("PAUSE");
 }
